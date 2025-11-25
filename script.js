04a275a986f49a301e215be85ecb2eaf885ce9ab
Anshul vishwakarma <anshul.vishwakarma1357@gmail.com>
2025-11-25 08:44:46 +0530

    updated abot/contact

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const counters = document.querySelectorAll('.counter');
+counters.forEach(counter => {
+    counter.innerText = '0';
+    const update = () => {
+        const target = +counter.getAttribute('data-target');
+        const current = +counter.innerText;
+        const increment = target / 100;
+
+        if (current < target) {
+            counter.innerText = Math.ceil(current + increment);
+            setTimeout(update, 20);
+        } else {
+            counter.innerText = target;
+        }
+    };
+    update();
+});
+
+
 fetch("footer.html")
     .then(r => r.text())
     .then(t => document.getElementById("footer").innerHTML = t);
